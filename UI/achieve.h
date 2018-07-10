61bb8223c5984912e0a2d49767847e1a295ff7ed
刘俞兰 <33798355+Zoelan@users.noreply.github.com>
2018-07-10 14:55:09 +0800

    add qrc

--- UI/achieve.h
+++ UI/achieve.h
@@ -3,7 +3,7 @@
 
 #include <QWidget>
 #include <QPainter>
-#include <QPIxmap>
+#include <QPixmap>
 #include "openpagebutton.h"
 
 namespace Ui {
@@ -29,6 +29,29 @@ private:
 
 	QPixmap back;
 	OpenPageButton *backButton = NULL;
+    QPixmap achieve1;
+    QPixmap achieve2;
+    QPixmap achieve3;
+    QPixmap achieve4;
+    QPixmap achieve5;
+    QPixmap achieve6;
+    QPixmap achieve7;
+    QPixmap achieve8;
+    QPixmap achieve9;
+    QPixmap achieve10;
+    QPixmap achieve11;
+    QPixmap achieve12;
+    QPixmap achieve13;
+    QPixmap achieve14;
+    QPixmap achieve15;
+    QPixmap achieve16;
+    QPixmap achieve17;
+    QPixmap achieve18;
+    QPixmap achieve19;
+    QPixmap achieve20;
+    QPixmap achieve21;
+    QPixmap achieve22;
+    QPixmap achieve23;
 };
 
 #endif // ACHIEVE_H
