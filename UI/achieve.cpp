c8691936a7dd6ea43a5632faa6ea7c66ad1e0c30
qpalwo <32657983+qpalwo@users.noreply.github.com>
2018-07-10 10:48:05 +0800

    forth game

--- UI/achieve.cpp
+++ UI/achieve.cpp
@@ -7,12 +7,20 @@ Achieve::Achieve(QWidget *parent) :
 	ui->setupUi(this);
 	setWindowFlag(Qt::FramelessWindowHint);
 	back.load(":/achieve/back.png");
+	backButton = new OpenPageButton(this, "/menuZ/menu/", "set_back_ruins");
+	backButton->setGeometry(830, 570, 70, 70);
+	connect(backButton, SIGNAL(clicked()), this, SLOT(onBackClicked()));
 }
+
 
 
 void Achieve::paintEvent(QPaintEvent *e) {
 	QPainter painter(this);
-	painter.drawPixmap(0, 0, 960, 720, back);
+	painter.drawPixmap(0, 0, back);
+}
+
+void Achieve::onBackClicked() {
+	close();
 }
 
 Achieve::~Achieve() {
--- game/booktoburn.h
+++ game/booktoburn.h
@@ -14,7 +14,7 @@ class BookToBurn : public QObject, public QGraphicsItem
 	Q_OBJECT
 
 public:
-    BookToBurn();
+    BookToBurn(int, int);
 	~BookToBurn();
 
 	QRectF boundingRect() const;
@@ -38,20 +38,33 @@ public slots:
 	void finishGame();
 	void reFresh();
 
+	void onPressing();
+
+protected:
+	void keyReleaseEvent(QKeyEvent *event);
+	void keyPressEvent(QKeyEvent *event);
+
 private:
 	QPixmap pixMap;
 	FireBurnBook * fireBurnBook = NULL;
-	QTimer *reFreshTimer;
+	QTimer *reFreshTimer = NULL;
+	QTimer *pressTimer = NULL;
 
 	QPainterPath shape() const;
 	void calculateX();
 	void calculateY();
 	void calculateVY();
 	void killMe();
+	void throughBook();
 
-	const float G = 1.5;
+	const float G = 2;
 	float vy;
 	float vx;
+
+	int nowTime = 0;
+	bool isRight = true;
+	bool isThrough = false;
+	bool isPressing = false;
 };
 
 #endif // BOOKTOBURN_H
